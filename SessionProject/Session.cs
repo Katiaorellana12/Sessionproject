9721256dd27f0a7c4dffb0eaf331b03b8a830c51
sofx10 <sofihernandez059@gmail.com>
2021-09-04 23:01:48 -0600

    rama de SMIS015421

--- SessionProject/Session.cs
+++ SessionProject/Session.cs
@@ -43,4 +43,47 @@ namespace SessionProject
 
 
         }
-      
+        public void deleteFile(String fileName)
+        {
+
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+            System.IO.File.Delete(fileName);
+
+        }
+
+        public Boolean existsFile(String fileName)
+        {
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+
+            return System.IO.File.Exists(fileName);
+
+        }
+
+
+
+        public async void deleteLine(int line, String fileName)
+        {
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+            string text = this.readFile(fileName);
+            string[] lines = text.Split('\n');
+
+
+            await File.WriteAllLinesAsync(fileName, lines);
+        }
+    }
+
+    class Session
+    {
+
+        String fileStoreCredentials = "parcialprogramacion.txt";
+        FileManager fileManager = null;
+        const string passwordAdmin = "123";
+        public Session()
+        {
+            this.fileManager = new FileManager();
+            if (!fileManager.existsFile(fileStoreCredentials))
+            {
+                fileManager.createFile(fileStoreCredentials);
+                fileManager.writeFile("admin:123", fileStoreCredentials);
+            }
+        }
