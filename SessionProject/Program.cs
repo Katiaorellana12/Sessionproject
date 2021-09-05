f1a4672ee01918332365d2c3d47b213fa7ffcd9a
Katiaorellana12 <88348464+Katiaorellana12@users.noreply.github.com>
2021-09-04 21:13:57 -0600

    Add files via upload

---
+++ SessionProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SessionProject
+{
+    class Program
+    {
+
+        static Session session = new Session();
+        static Boolean isAdmin = false;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<INICIO DE SESION>");
+            login();
+
+        }
+
+
+      
+
+        
---
+++ SessionProject/Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SessionProject
+{
+    class FileManager
+    {
+        String pathDirectory = "";
+        public FileManager()
+        {
+
+            DirectoryInfo directoryInfo = System.IO.Directory.CreateDirectory(".\\store");
+            pathDirectory = directoryInfo.FullName;
+
+        }
+
+      
